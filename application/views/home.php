acf6639a9b9fd0903b09570e4a21d34294533745
kun <okisyima7@gmail.com>
2018-10-21 06:19:14 +0700

    woyo

---
+++ application/views/home.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'layout/head.php';
+require_once 'layout/body.php';
+require_once 'layout/footer.php';
+require_once 'layout/script.php';
+
+
+ ?>
---
+++ application/views/layout/head.php
@@ -0,0 +1,35 @@
+<!-- Start of HEAD -->
+
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+<head>
+  <!-- Mobile Specific Meta -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="img/fav.png">
+  <!-- Author Meta -->
+  <meta name="author" content="colorlib">
+  <meta name="description" content="">
+  <!-- Meta Description -->
+  <!-- Meta Keyword -->
+  <meta name="keywords" content="">
+  <!-- meta character set -->
+  <meta charset="UTF-8">
+  <!-- Site Title -->
+  <title>Repair</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+    <!--
+    CSS
+    ============================================= -->
+    <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet"  type='text/css'>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/nice-select.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/animate.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/owl.carousel.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/main.css">
+  </head>
+
+<!--- End of HEAD -->
